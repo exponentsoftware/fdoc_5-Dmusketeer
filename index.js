3140b90ad3db306b3eea3756f703714fd28d580c
Dmusketeer <ardmountronics@gmail.com>
2021-10-07 13:59:53 +0530

    10 largest countries

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+
 const axios = require('axios')
+import fetch from 'node-fetch';
+const response = await fetch("https://restcountries.com/v2/all");
+const data = await response.json();
+const languages = new Set();
+const area = []
+
+
+//  How many languages are there in the countries API
 axios.get('https://restcountries.com/v3/all')
     .then(resp => {
         let details = resp.data;
@@ -15,3 +24,29 @@ axios.get('https://restcountries.com/v3/all')
         // Handle Error Here
         console.error(err);
     });
+
+
+
+
+// Find the 10 most largest countries
+function allLang(data) {
+    for (let Obj of data) {
+        if (Obj.languages) {
+            Object.values(Obj.languages).forEach(e => languages.add(e))
+        }
+    }
+    var size = languages.size;
+    console.log(languages);
+}
+// allLang(data)
+
+function area10() {
+    for (let obj of data) {
+        if (obj.area) {
+            area.push({ country: obj.name, Area: obj.area })
+        }
+
+    } console.log(area)
+}
+area10()
+
